4794dfc1ed9e65245f142bb86b9b1969a0618382
Chase Armstrong <chasejarms@gmail.com>
2019-06-12 14:25:32 -0600

    Continue adding styling to the main posts page

--- app/public/wp-content/themes/chasearmstrongblog/index.php
+++ app/public/wp-content/themes/chasearmstrongblog/index.php
@@ -1,17 +1,19 @@
 <?php get_header(); ?>
 
-<div id="posts">
-    <?php
-        while(have_posts()) {
-            the_post(); ?>
-            <a href="<?php the_permalink() ?>">
-                <div class="post-container">
-                    <h2 ><?php the_title() ?></h2>
-                    <p><?php the_content() ?></p>
-                </div>
-            </a>
-        <?php }
-    ?>
+<div id="posts-page">
+    <div class="posts-container">
+        <?php
+            while(have_posts()) {
+                the_post(); ?>
+                <a href="<?php the_permalink() ?>" class="post-container-wrapper">
+                    <div class="post-container">
+                        <h2 class="post-container-title"><?php the_title() ?></h2>
+                        <p><?php the_excerpt() ?></p>
+                    </div>
+                </a>
+            <?php }
+        ?>
+    </div>
 </div>
 
 <?php get_footer(); ?>
