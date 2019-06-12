95659b0415362d33dca752f7c6b916c83d21ca12
Chase Armstrong <chasejarms@gmail.com>
2019-06-12 15:06:52 -0600

    Make the index page look like the old index page

--- app/public/wp-content/themes/chasearmstrongblog/functions.php
+++ app/public/wp-content/themes/chasearmstrongblog/functions.php
@@ -1,5 +1,7 @@
 <?php
     function chase_armstrong_files() {
+        wp_enqueue_style('roboto_font', '//fonts.googleapis.com/css?family=Roboto:100,300,400');
+        wp_enqueue_style('open_sans_font', '//fonts.googleapis.com/css?family=Open+Sans:300,600');
         wp_enqueue_style('main_stylesheet', get_stylesheet_uri());
     }
 
--- app/public/wp-content/themes/chasearmstrongblog/index.php
+++ app/public/wp-content/themes/chasearmstrongblog/index.php
@@ -7,8 +7,17 @@
                 the_post(); ?>
                 <a href="<?php the_permalink() ?>" class="post-container-wrapper">
                     <div class="post-container">
-                        <h2 class="post-container-title"><?php the_title() ?></h2>
-                        <p><?php the_excerpt() ?></p>
+                        <div class="date-container">
+                            <p>
+                                <?php echo get_the_date('m.d.Y') ?>
+                            </p>
+                        </div>
+                        <div class="title-and-excerpt-container">
+                            <h2 class="post-container-title"><?php the_title() ?></h2>
+                            <div class="excerpt-container">
+                                <?php the_excerpt() ?>
+                            </div>
+                        </div>
                     </div>
                 </a>
             <?php }
