5fe0980a01477a1d3f4dc12ca203245e48a391c6
Chase Armstrong <chasejarms@gmail.com>
2019-06-15 00:37:08 -0600

    Continue working on single php file

--- app/public/wp-content/themes/chasearmstrongblog/single.php
+++ app/public/wp-content/themes/chasearmstrongblog/single.php
@@ -1,11 +1,28 @@
 <?php get_header() ?>
-
-<?php
-    while(have_posts()) {
-        the_post(); ?>
-        <h2><?php the_title() ?></h2>
-        <p><?php the_content() ?></p>
-    <?php }
-?>
-
+<div class="single-post-container">
+    <?php
+        while(have_posts()) {
+            the_post(); ?>
+            <div class="single-post-header">
+                <a href="/" class="to-posts-anchor">
+                    <div class="to-posts-container">
+                        <p class="to-posts-text"><</p>
+                        <p class="to-posts-text not-carrot">TO ARTICLES</p>
+                    </div>
+                </a>
+            </div>
+            <hr class="single-post-hr"/>
+            <p class="single-post-date">
+                <?php
+                    echo get_the_date('m.d.Y');
+                ?>
+            </p>
+            <h1 class="single-post-title">
+                <?php
+                    the_title()
+                ?>
+            </h1>
+        <?php }
+    ?>
+</div>
 <?php get_footer() ?>
