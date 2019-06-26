9bb8eddf3f3318b54b8174c67ae0a5514bb79acb
Chase Armstrong <chasejarms@gmail.com>
2019-06-26 11:22:32 -0600

    Add tracking code to header.php

--- app/public/wp-content/themes/chasearmstrongblog/header.php
+++ app/public/wp-content/themes/chasearmstrongblog/header.php
@@ -4,5 +4,14 @@
         <?php wp_head(); ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?php wp_title(); ?></title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142760397-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-142760397-1');
+        </script>
     </head>
     <body>
