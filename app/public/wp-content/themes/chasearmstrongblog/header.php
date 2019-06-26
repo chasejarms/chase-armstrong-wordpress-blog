9a21ebb0828c3afdc7a8f5e42d560e06e7c55e11
Chase Armstrong <chasejarms@gmail.com>
2019-06-26 09:26:28 -0600

    Use site title and tagline in site title

--- app/public/wp-content/themes/chasearmstrongblog/header.php
+++ app/public/wp-content/themes/chasearmstrongblog/header.php
@@ -3,5 +3,6 @@
     <head>
         <?php wp_head(); ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php wp_title(); ?></title>
     </head>
     <body>
